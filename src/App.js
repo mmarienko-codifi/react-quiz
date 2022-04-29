e4dabf7d523dc686ead107c48327561f5e2a907c
mmarienko <maksym.marienko@gmail.com>
2022-04-29 17:26:04 +0300

    ActiveQuiz, Answers, Menu, Backdrop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
+import Layout from './hoc/Layout/Layout'
+import Quiz from './containers/Quiz/Quiz'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello world!</h1>
+      <Layout>
+        <Quiz />
+      </Layout>
     )
   }
 }
