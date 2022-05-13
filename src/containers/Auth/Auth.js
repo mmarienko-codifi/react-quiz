d74d4a5e1c3b05557065bcba0d76f9ead06fcc69
mmarienko <maksym.marienko@gmail.com>
2022-05-13 16:04:58 +0300

    add key

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -41,10 +41,10 @@ export default class Auth extends Component {
     const authData = {
       email: this.state.formControls.email.value,
       password: this.state.formControls.password.value,
-      returnSecureToken: true
+      returnSecureToken: false
     }
     try {
-      const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBRj1R0UEHzbzdDaOOQIhjqWUvDsusN4Mo', authData)
+      const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAS1J76q6viA4vhNk-EHwAPPi7KV-qdUZQ', authData)
 
       console.log(response.data)
     } catch (e) {
@@ -59,7 +59,7 @@ export default class Auth extends Component {
       returnSecureToken: true
     }
     try {
-      const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBRj1R0UEHzbzdDaOOQIhjqWUvDsusN4Mo', authData)
+      const response = await axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAS1J76q6viA4vhNk-EHwAPPi7KV-qdUZQ', authData)
 
       console.log(response.data)
     } catch (e) {
